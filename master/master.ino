776f09b1d2fa510a22e95116a3c1a2f3d18abedd
gabriexpo <gabriexpo@libero.it>
2022-02-27 19:26:52 +0100

    Fix per mqtt connection e per il cast dei float (per salvare su EEPROM)

--- master/master.ino
+++ master/master.ino
@@ -7,12 +7,12 @@
 // EspMQTTClient init
 
 EspMQTTClient client(
-  "ASUS",           // WIFI SSID
+  WIFI,             // da costanti.h
   PASSWORD,         // da costanti.h
-  "192.168.1.100",  // MQTT Broker server ip
-  "MQTTUsername",   // Can be omitted if not needed
-  "MQTTPassword",   // Can be omitted if not needed
-  "ESP8266",        // Client name that uniquely identify your device
+  "192.168.1.54",  // MQTT Broker server ip
+//  "MQTTUsername",   // Omesso dato che uso una connessione anonima
+//  "MQTTPassword",   // Omesso dato che uso una connessione anonima
+  "ESP8266",        // Client name
   1883              // The MQTT port, default to 1883. this line can be omitted
 );
 
@@ -74,8 +74,10 @@ void setup() {
   // Setting up i2c connection
   Wire.begin(SDA_PIN, SCL_PIN, I2C_MASTER);
 
-  // Setting up last message to MQTT server
-  client.enableLastWillMessage("ESP8266/piggy-bank", "Piggy-bank going offline!");
+  // Setting MQTT related options
+  client.enableDebuggingMessages();
+  client.enableMQTTPersistence();
+  //client.enableLastWillMessage("ESP8266/piggy-bank", "Piggy-bank going offline!");
 
   // Setting up base value for measurement
   setBaseValue();
@@ -86,9 +88,11 @@ void setup() {
 // ----
 
 void loop() {
+
+  client.loop();
   
   int currSensorValue = analogRead(A0);
-  if (abs(currSensorValue - sensorValue) > 5) {
+  if (abs(currSensorValue - baseValue) > 20) {        // 20 valore fissato per indicare un cambiamento significativo nello stato del potenziometro
     Serial.println(currSensorValue);
     
     measurement();
@@ -97,18 +101,24 @@ void loop() {
     
     // update total
     if (coin != "nan") {
-        total += coin.toFloat();
+      total += coin.toFloat();
+      //Serial.println(coin.toFloat());
+      Serial.print("Total: ");
+      Serial.println(total);
+
+      // EEPROM
+      EEPROM.put(0, total);
+      boolean res = EEPROM.commit();
+      Serial.println((res) ? "Commit ok" : "Commit failed");
+
+      // send to mqtt
+      client.publish("ESP8266/piggy-bank", "Inserted coin: " + coin);
     }
-    EEPROM.put(0, total);
-    boolean res = EEPROM.commit();
-    Serial.println((res) ? "Commit ok" : "Commit failed");
-    
-    // send to mqtt
-    client.publish("ESP8266/piggy-bank", "Inserted coin: " + coin);
     
     // send to slave
     i2c_Transmission(coin);
-    
+
+    delay(500);
     // reposition lever
     repositionLever();
     Serial.println("Fine");
@@ -127,6 +137,8 @@ void onConnectionEstablished() {
   client.publish("ESP8266/piggy-bank", "Piggy-bank online!");
 }
 
+// Funzione per stabilire il valore "a riposo" che percepisce il potenziometro.
+
 void setBaseValue() {
   int sum = 0;
   int current = 0;
@@ -134,13 +146,13 @@ void setBaseValue() {
   for(int i = 0; i < 10; i++) {   // prime read per togliere dei risultati strani
     delay(100);
     current = analogRead(A0);
-    Serial.println(current);
+    //Serial.println(current);
   }
   
   for(int i = 0; i < 10; i++) {   // read per calcolare il valore base
     delay(100);
     current = analogRead(A0);
-    Serial.println(current);
+    //Serial.println(current);
     sum += current;
   }
   
@@ -151,11 +163,18 @@ void setBaseValue() {
   Serial.println(baseValue);
 }
 
+// Funzione per trasmettere, sotto forma di stringa, il valore della moneta
+// inserita alla board slave.
+
 void i2c_Transmission(String message) {
   Wire.beginTransmission(I2C_SLAVE);  // transmit to slave
   Wire.write(message.c_str());
   Wire.endTransmission();    // stop transmitting
 }
+
+// Funzione che aspetta che l'utente finisca di far passare la moneta
+// tra la barra di misurazione e la parete di sostegno e che termina 
+// ponendo SensorValue al valore raggiunto dal passaggio della moneta.
 
 void measurement() {
   Serial.println("Measurement:");
@@ -167,30 +186,36 @@ void measurement() {
     int currSensorValue = analogRead(A0);
     diff = abs(sensorValue - currSensorValue);
     sensorValue = currSensorValue;
-    Serial.println(sensorValue);
+    //Serial.println(sensorValue);
   } while(diff > 3);
   
 }
+
+// Funzione che trasforma il valore del potenziometro (nella variabile sensorValue)
+// in una stringa contenente il valore della moneta secondo le soglie definite 
+// nelle prime righe del codice
 
 String getCoin() {
   String coin;
   
   if (sensorValue >= MIN10c && sensorValue <= MAX10c) {
-    coin = "0,10";
+    coin = "0.10";
   } else if (sensorValue >= MIN20c && sensorValue <= MAX20c) {
-    coin = "0,20";
+    coin = "0.20";
   } else if (sensorValue >= MIN1e && sensorValue <= MAX1e) {
-    coin = "1,00";
+    coin = "1.00";
   } else if (sensorValue >= MIN50c && sensorValue <= MAX50c) {
-    coin = "0,50";
+    coin = "0.50";
   } else if (sensorValue >= MIN2e && sensorValue <= MAX2e) {
-    coin = "2,00";
+    coin = "2.00";
   } else {
     coin = "nan";
   }
 
   return coin;
 }
+
+// Funzione per riposizionare la leva che aziona il potenziometro dopo una misurazione
 
 void repositionLever() {
   Serial.println("Reposition lever:");
@@ -203,7 +228,7 @@ void repositionLever() {
     stepsTaken += 2;
     current = analogRead(A0);
     sensorValue = current;
-    Serial.println(current);
+    //Serial.println(current);
     delay(1);
   }
 
